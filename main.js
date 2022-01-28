129975c13c26320b9d0d7d1941daa833de87629e
lcryan <lcryan06@gmail.com>
2022-01-28 16:56:07 +0100

    impl JS in HTML header added text added

--- main.js
+++ main.js
@@ -207,9 +207,6 @@ console.log(pricesTV);
 // Tip: wanneer we meerdere waardes uit een array willen terugbrengen tot één getal of één string,
 // gebruik je hier gewoon een oude vertrouwde for-loop voor!
 //
-// Opdracht 2a: Hoeveel tv's zijn er al verkocht? Schrijf een script dat dit berekent.
-// Log de uitkomst in de console.
-// Opdracht 2b: Zorg ervoor dat dit aantal in het groen wordt weergegeven op de pagina.
 // Opdracht 2c: Hoeveel tv's heeft Tech It Easy ingekocht? Schrijf een script dat dit berekent.
 // Log de uitkomst in de console.
 // Opdracht 2d: Zorg ervoor dat dit aantal in het blauw wordt weergegeven op de pagina.
