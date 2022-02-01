b584819b36cd11a50ee2ab9163a95670a216ea6c
lcryan <lcryan06@gmail.com>
2022-02-01 17:13:49 +0100

    finished up to 4b.

--- main2.js
+++ main2.js
@@ -9,28 +9,31 @@
 
 //Opdracht 3a.const
 
-list = document.getElementById('ListOfTvs');
+//const list = document.getElementById('ListOfTvs');
 
-inventory.map((listOfTvs) => {
-    const actualList = document.createElement('li')
-    actualList.textContent = listOfTvs.brand;
+//inventory.map((listOfTvs) => {
+//    const actualList = document.createElement('li')
+//    actualList.textContent = listOfTvs.brand;
 
-    return list.appendChild(actualList);
-});
+//    return list.appendChild(actualList);
+//});
 //einde 3a. -------------------------------- //
-
 
 
 //3b.
 
-function newFunction(tvBrands) {
-    const allTvNames = inventory.map((listOfTvs) => {
-        return listOfTvs.brand;
+function listOfTvBrands(array) {
+    const list = document.getElementById('ListOfTvs');
+
+    array.map((listOfTvs) => {
+        const actualList = document.createElement('li')
+        actualList.textContent = listOfTvs.brand;
+
+        return list.appendChild(actualList);
     })
-    console.log(allTvNames);
 }
 
-newFunction();
+    listOfTvBrands(inventory);
 
 //eind van 3b. ------------------//
 
@@ -40,11 +43,46 @@ newFunction();
 // verwacht en de naam op de volgende manier samenvoegt: [merk] [type] - [naam] zoals
 // Philips 43PUS6504/12 - 4K TV of NIKKEI NH3216SMART - HD smart TV.
 // Test of jouw functie ook werkt wanneer er een ander tv object wordt meegegeven.
-//
+//Logica :
+//1. Schreibe eine Funktion und benutze array methode map ?? Und dann in string?? Recherche!
+// . Marke, Typ und Name muesen wie oben
+//dargestellt werden. - brand, name, type
+//2. Log und teste ob es geht.
+//3. Log die Funktion mit einem anderen Fernseher, um nochmal zu testen, ob es auch auf andere andgewendet
+//werden kann.
+
+//possibility 1 ;
+//function firstObject() {
+//    return inventory[5].brand + ' ' + inventory[5].type + ' ' +  '-' + ' ' + inventory[5].name;
+//}
+//console.log(firstObject());
+
+// possibility 2 ;
+function getTvOut(tvDetails){
+    return `${tvDetails.brand}  ${tvDetails.type} - ${tvDetails.name}`;
+}
+
+console.log(getTvOut(inventory[6]));
+
+//eind opdracht 4a.---------------------------//
+
+
 // Opdracht 4b: Zorg ervoor dat de prijs van een tv netjes geformat wordt.
 // Maak een functie die één tv-prijs als parameter verwacht (zoals 379) en daar de volgende string van maakt:
 // €379,-. Test of jouw functie ook werkt wanneer er een andere tv-prijs wordt meegegeven.
-//
+
+//1. function like above, but extract price. Maybe for the same TV.
+//2. Add Eurosign and ,-
+//3. Check out code with another TV element
+
+function tvPrice(tvDetails) {
+    return `€ ${tvDetails.price}` + ',-';
+}
+
+console.log(tvPrice(inventory[6]));
+// eind opdracht 4b. --------------------------------------//
+
+
 //Opdracht 4c: Zorg ervoor dat er een string wordt gegenereerd voor alle beschikbare schermgroottes van één tv
 // , in zowel inches als cm. Doe dit door een functie te schrijven die één screen-sizes array verwacht
 // ( zoals inventory[0].availableSizes) en de groottes op de volgende manier samenvoegt: [schermgrootte]
@@ -52,9 +90,23 @@ newFunction();
 // Als een tv maar één schermgrootte heeft ([32]) wordt de output 32 inch (81 cm).
 // Wanneer een tv vier schermgroottes heeft ([43, 50, 55, 58]) wordt de output 43 inch (109 cm) | 50 inch
 // (127 cm) | 58 inch (147 cm). Test of jouw functie werkt in alle mogelijke gevallen.
-//Opdracht 4d: Zorg ervoor de informatie van één van de tv's zoals het voorbeeld wordt weergegeven op de
+
+
+
+
+
+// Opdracht 4d: Zorg ervoor de informatie van één van de tv's zoals het voorbeeld wordt weergegeven op de
 // pagina. Gebruik hiervoor de functies die je hebt gemaakt in opdracht 5a, 5b en 5c.
-//Opdracht 4e: Schrijf een functie die ALLE tv's weergeeft op de pagina zoals in het voorbeeld.
+
+
+
+
+
+
+
+
+
+// Opdracht 4e: Schrijf een functie die ALLE tv's weergeeft op de pagina zoals in het voorbeeld.
 // Dit wil je natuurlijk niet acht keer opnieuw schrijven, want nu zijn het 8 tv's,
 // maar in de toekomst misschien wel 200! Gebruik in deze functie de voorgaande functies die je hebt geschreven,
 // om onderdelen van de data te formatten.
